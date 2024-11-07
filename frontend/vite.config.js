a96cbb4db9f4920790cdacce4b956d4a78147014
jhonatanareiza1 <jhonatanareiza1@hotmail.com>
2024-11-07 12:56:29 -0500

    Agregado soporte para puerto y host en la configuración de Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173,  // Usa el puerto proporcionado por Render o 5173 por defecto
+    host: true,                      // Permite acceder a la app desde cualquier IP
+  },
 })
