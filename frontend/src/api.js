3e92a780b1e3958868d7c15dbb94c9f9c0ae03fb
jhonatanareiza1 <jhonatanareiza1@hotmail.com>
2024-11-09 19:15:01 -0500

    cabios en api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// Selecciona la URL base según el entorno (desarrollo o producción)
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.MODE === 'development' 
+    ? import.meta.env.VITE_API_URL_DEV 
+    : import.meta.env.VITE_API_URL_PROD 
 });
 
 export default api;
